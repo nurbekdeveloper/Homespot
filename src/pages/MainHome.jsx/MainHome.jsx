747f589fea1e9fdbd1152542368ee2ff1ef1b37c
FIRST_NAME LAST_NAME <MY_EMAIL@SOME_DOMAIN.com>
2024-05-30 16:25:34 +0500

    add

---
+++ src/pages/MainHome.jsx/MainHome.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import BuildingTypes from '../../componenets/types/BuildTypes'
+import UsefulLinks from '../../componenets/usefulLinks/UsefulLinks'
+import ContactForm from '../../componenets/form/ContactForm'
+
+function Construction() {
+  return (
+    <div>
+         <BuildingTypes/>
+         <UsefulLinks/>
+         <ContactForm/>
+    </div>
+  )
+}
+
+export default Construction
