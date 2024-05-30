218d1f4ebd964669bd35bba9692f5160fa80c782
FIRST_NAME LAST_NAME <MY_EMAIL@SOME_DOMAIN.com>
2024-05-30 11:48:36 +0500

    ad snsa

--- src/componenets/types/BuildingTypeCard.jsx
+++ src/componenets/types/BuildingTypeCard.jsx
@@ -6,7 +6,7 @@ import { useNavigate } from 'react-router-dom';
 const BuildingTypeCard = ({ image, count, title,url }) => {
     const navigate = useNavigate()
     return (
-        <div className="card" onClick={()=>navigate('/construction'+ url)}>
+        <div className="cardb" onClick={()=>navigate('/construction'+ url)}>
             <div className="badge">{count}</div>
             <img src={image} alt={title} className="card-image" />
             <div className="card-title">{title}</div>
