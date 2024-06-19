62383f4d6fa60c1e9ed58c7fddfe68fe6a15a5b3
nurbekdeveloper <nurbeknormatov_02@gmail.com>
2024-06-19 16:51:25 +0500

    add new

--- src/componenets/project/Card.jsx
+++ src/componenets/project/Card.jsx
@@ -3,7 +3,7 @@ import './Card.css';
 
 const Card = ({ image, title, technology, dimensions, price }) => {
     return (
-        <div className="card">
+        <div className="card-ready">
             <img src={image} alt={title} className="card-image" />
             <div className="card-content">
                 <h3 className="card-title">{title}</h3>
